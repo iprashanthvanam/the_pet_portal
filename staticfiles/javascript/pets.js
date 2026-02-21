d457756fda26d21359fb28e6f477334501717213
VANAM PRASHANTH <prashanthvanam61@gmai.com>
2026-02-21 12:14:04 +0530

    Removed staticfiles from repository

--- staticfiles/javascript/pets.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-
-    /* 1. Mobile Menu Toggle */
-    window.toggleMenu = function() {
-        const navLinks = document.getElementById('navLinks');
-        navLinks.classList.toggle('active');
-    }
